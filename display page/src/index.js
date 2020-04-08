8a256572fa0adaa402af29a6f970028e087236eb
JWang169 <lo_vegood@126.com>
2020-04-08 10:00:32 -0400

    :package: show tutor list

--- display page/src/index.js
+++ display page/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
-
+import faker from 'faker';
 
 const App = () =>{
     return (
         <div className="ui container comments">
-            <ApprovalCard><CommentDetail author="Claire" timeAgo="Everyday at 2:00 PM" content="Web Programming" /></ApprovalCard >
-            <ApprovalCard><CommentDetail author="Joey" timeAgo="Tuesday at 3:00 PM" content="Chinese"/></ApprovalCard >
-            <ApprovalCard><CommentDetail author="Mac" timeAgo="Wednesday at 12:00 PM" content="Database"/></ApprovalCard >
-            <ApprovalCard><CommentDetail author="Kyle" timeAgo="Wednesday at 12:00 PM" content="Machine Learning"/></ApprovalCard >
-            <ApprovalCard><CommentDetail author="Vic" timeAgo="Friday at 10:00 AM" content="Biology"/></ApprovalCard >
+            <ApprovalCard><CommentDetail author="Sybill Trelawney" timeAgo="Tuesday at 3:00 PM" content="Divination" avatar={faker.image.avatar()} /></ApprovalCard >
+            <ApprovalCard><CommentDetail author="Alastor Moody" timeAgo="Wednesday at 12:00 PM" content="Defence Against the Dark Arts" avatar={faker.image.avatar()} /></ApprovalCard >
+            <ApprovalCard><CommentDetail author="Minerva McGonagall" timeAgo="Everyday at 2:00 PM" content="Transfiguration" avatar={faker.image.avatar()} /></ApprovalCard >
+            <ApprovalCard><CommentDetail author="Severus Snape" timeAgo="Wednesday at 12:00 PM" content="Potions" avatar={faker.image.avatar()} /></ApprovalCard >
+            <ApprovalCard><CommentDetail author="Rolanda Hooch" timeAgo="Friday at 10:00 AM" content="Flying" avatar={faker.image.avatar()} /></ApprovalCard >
+            
            
         </div>
     )
