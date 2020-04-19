1d4652c558799b4c823f4eccf5d367db41029572
JWang169 <lo_vegood@126.com>
2020-04-19 11:49:57 -0400

    :package: add personal page and demo

--- studygeekreact/src/components/App.js
+++ studygeekreact/src/components/App.js
@@ -6,12 +6,14 @@ import 'semantic-ui-css/semantic.min.css';
 import '../App.css';
 
 import Homepage from './Homepage';
+import PersonalPage from './PersonalPage';
 import Navigation from './Navigation';
 import SignUp from './SignUp';
 import LogIn from './Login';
 import SearchStudents from './SearchStudents';
 import SearchTutors from './SearchTutors';
 import TutorInfo from './TutorInfo';
+import Demo from './Demo';
 
 
 function App() {
@@ -19,9 +21,11 @@ function App() {
     <Router>
       <Container>
         <Navigation />
+        {/* <Demo /> */}
         <Route exact path='/' component={Homepage} />
         <Route exact path='/signup' component={SignUp} />
         <Route exact path='/login' component={LogIn} />
+        <Route exact path='/demo' component={Demo} />
         <Route exact path='/searchtutors' component={SearchTutors} />
         <Route exact path='/searchstudents' component={SearchStudents} />
       </Container>
