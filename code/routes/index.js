0e8e63cbbfe3a6e86b3f313e1066097a5e7ce267
JWang169 <lo_vegood@126.com>
2020-04-29 00:52:13 -0400

    :package: signup page

--- code/routes/index.js
+++ code/routes/index.js
@@ -3,6 +3,17 @@ const tutorRoutes = require('./tutors');
 const handler = app => {
     app.use('/tutors', tutorRoutes);
 
+
+    app.use('/signup', (req, res) => {
+        res.render("studygeeks/signup", {});
+    });
+
+    app.use('/login', (req, res) => {
+        res.render("studygeeks/login", {});
+    });
+    app.use('/logout', (req, res) => {
+        res.render("studygeeks/logout", {});
+    });
     app.use('*', (req, res) => {
         res.render("studygeeks/homepage", {});
     });
