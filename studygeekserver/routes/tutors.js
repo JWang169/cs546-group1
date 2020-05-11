f3bc42ccee8858327cb8ee4b01d11aed098407b3
charkood <32330434+charkood@users.noreply.github.com>
2020-05-11 13:55:11 -0400

    Update tutors.js

--- studygeekserver/routes/tutors.js
+++ studygeekserver/routes/tutors.js
@@ -3,52 +3,116 @@ const router = express.Router();
 const data = require("../data");
 const tutorData = data.tutors;
 
-
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const tutorList = await tutorData.getAlltutors();
-    res.json(tutorList);
+    let tutor = await tutorData.getAlltutors();
+    res.json(tutor);
   } catch (e) {
-    // Something went wrong with the server!
-    res.status(500).send();
+    res.sendStatus(500);
   }
 });
 
-// update tutor info 
-router.put("/:id", async (req, res) => {
-  console.log('in routes/tutors.js')
-  const info = req.body['info'];
-  const subjects = req.body['subjects'];
-  if(!info){
-    res.status(400).json({error: "No Tutor info provided."})
-    return 
-  }
-  if(!subjects){
-    res.status(400).json({error: "No Tutor subjects provided."})
-    return 
-  }
-
-  try{
-    const updatedTutor = await tutorData.updateTutor(req.params.id, info, subjects);
-    res.status(200).json(updatedTutor);
-  }catch(e){
-    console.log(e)
-    res.status(500).json({error: e});
-  }
-});
-
-
-//GET /tutors/{id}
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    // console.log('in get tutors')
-    const tutor = await tutorData.getTutor(req.params.id);
-    // console.log(tutor)
-    res.status(200).send(tutor);
+    let tutor = await tutorData.getTutor(req.params.id);
+    res.json(tutor);
   } catch (e) {
-    console.log(e)
-    res.status(404).json({ message: "Tutor not found!" });
+    res.status(404).json({ error: 'Tutor not found' });
   }
 });
+
+router.get('/email/:email', async (req, res) => {
+  try {
+    let tutor = await tutorData.getTutorByEmail(req.params.email);
+    res.json(tutor);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+router.get('/pricehightolow/:subject', async (req, res) => {
+  try {
+    let tutor = await tutorData.getTutorByPriceHighToLow(req.params.subject);
+    res.json(tutor);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+router.get('/pricelowtohigh/:subject', async (req, res) => {
+  try {
+    let tutor = await userData.getTutorByPriceLowToHigh(req.params.subject);
+    res.json(tutor);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+router.get('/townstate/:town/:state', async (req, res) => {
+  try {
+    let tutor = await tutorData.getTutorByTownState(req.params.town,req.params.state);
+    res.json(tutor);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+router.get('/ratehightolow/', async (req, res) => {
+  try {
+    let Tutor = await tutorData.getTutorByRatingHighToLow();
+    res.json(user);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+router.get('/ratelowtohigh/', async (req, res) => {
+  try {
+    let tutor = await tutorData.getTutorByRatingLowToHigh();
+    res.json(tutor);
+  } catch (e) {
+    res.status(404).json({ error: 'Tutor not found' });
+  }
+});
+
+
+// update tutor info
+// router.put("/:id", async (req, res) => {
+//   console.log('in routes/tutors.js')
+//   const info = req.body['info'];
+//   const subjects = req.body['subjects'];
+//   if(!info){
+//     res.status(400).json({error: "No Tutor info provided."})
+//     return
+//   }
+//   if(!subjects){
+//     res.status(400).json({error: "No Tutor subjects provided."})
+//     return
+//   }
+
+//   try{
+//     const updatedTutor = await tutorData.updateTutor(req.params.id, info, subjects);
+//     res.status(200).json(updatedTutor);
+//   }catch(e){
+//     console.log(e)
+//     res.status(500).json({error: e});
+//   }
+// });
+
+// router.post
+
+
+// //GET /tutors/{id}
+// router.get("/:id", async (req, res) => {
+//   try {
+//     // console.log('in get tutors')
+//     const tutor = await tutorData.getTutor(req.params.id);
+//     // console.log(tutor)
+//     res.status(200).send(tutor);
+//   } catch (e) {
+//     console.log(e)
+//     res.status(404).json({ message: "Tutor not found!" });
+//   }
+// });
 
 module.exports = router;
