4830823b8b7a5fa30f116b27c7e0a1498cbfec54
dovsiew <david.ovsiew@gmail.com>
2020-05-13 00:13:36 -0400

    fixed slight problem in routes\index.js

--- studygeekserver/routes/index.js
+++ studygeekserver/routes/index.js
@@ -1,12 +1,10 @@
 const tutorRoutes = require('./tutors');
 const studentRoutes = require('./students');
-const pairRoutes = require('./pairs');
 //const userRoutes = require('./users');
 
 const constructorMethod = (app) => {
 	app.use('/tutors', tutorRoutes);
 	app.use('/students', studentRoutes);
-	app.use('/pairs', pairRoutes);
 	// app.use('/', userRoutes);
 
 	app.use('*', (req, res) => {
